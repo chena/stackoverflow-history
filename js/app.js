6001858bba993768b62428ea21f89be9d5407e2d
Alice Chen <chiachun78@gmail.com>
2014-05-08 22:15:39 -0700

    Added tagcloud directive and visualization

--- js/app.js
+++ js/app.js
@@ -30,15 +30,11 @@ app.factory('HistoryService', function(StackExchangeService) {
 			var start = (new Date).getTime() - microseconds;
 			var questions = {}; // object the hold question URLs and their tags
 
-			// TODO: ng-click tags then categorize
-			// TODO: autocomplete search
-			// TODO: clear search
-			// TODO: topic cloud with D3
-			// TODO: tabs?
+			// TODO: ng-click tags to show count
 			chrome.history.search({
 				'text' : 'stackoverflow.com/questions', // look for visits from stackoverflow
 				'startTime' : start,
-				'maxResults' : 10
+				'maxResults' : 30
 			}, function(historyItems) {
 				historyItems.forEach(function(item, i) {
 					var url = item.url, 
@@ -75,34 +71,77 @@ app.factory('HistoryService', function(StackExchangeService) {
 });
 
 app.controller('PageController', function($scope, HistoryService) {
+	$scope.view = 'history';
+
 	HistoryService.search(function(questions) {
-		$scope.toArray = function(map) {
-			var array = [];
-			for (var key in map) {
-				array.push(map[key]);
-			}
-			return array;
-		}
+		$scope.setQuestions(questions);
+	});
 
-		$scope.search = function(tag) {
-			$scope.keyword = tag;
-		}
-
+	$scope.setQuestions = function(questions) {
 		// wrap in apply so angular knows when to bind our data when they become available
 		$scope.$apply(function() {
 			$scope.questions = questions;
-			// TODO: get all tags 
 		});
-	});
+	}
+
+	$scope.toArray = function(map) {
+		var array = [];
+		for (var key in map) {
+			array.push(map[key]);
+		}
+		return array;
+	}
+
+	$scope.search = function(tag) {
+		$scope.keyword = tag;
+	}
+
 });
 
-// word cloud directive
-/*
-app.directive('wordcloud', function() {
-	return {
-		restrict: 'EA', // restrict to element or attribute
-		scope: {
-			words: '='
-		}, 
+app.controller('WordCloudController', function($scope) {
+	$scope.setTagsData = function() {
+		var colors = d3.scale.category20();
+		var tags = {};
+
+		$scope.toArray($scope.questions).forEach(function(question) {
+			question.tags.forEach(function(tag) {
+				if (tags[tag]) {
+					tags[tag] += 1;
+				} else {
+					tags[tag] = 1;
+				}
+			});
+		});
+
+		$scope.tags = Object.keys(tags).map(function(tag, i) {
+			var count = tags[tag],
+				size = 10 + tags[tag]/10 * 80 + 'px';
+
+			return {
+				text: tag,
+				count: count, 
+				size: size, 
+				color: colors(i)
+			};
+		});
 	};
-});*/
+
+	$scope.showCount = function(tag) {
+		console.log(tag.count);
+	};
+
+	$scope.$watch('view', function() {
+		if ($scope.view === 'cloud') {
+			$scope.setTagsData();
+		}
+	})
+	
+});
+
+// word cloud directive for our tags
+app.directive('tagcloud', function() {
+	return {
+		restrict: 'E', // restrict to element
+		templateUrl: '../tagcloud.html'
+	};
+});
